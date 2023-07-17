522f07f24272084908e2f8239016abf5ff8a71b0
YoussefAGS <ya023560@gmail.com>
2023-07-17 23:39:06 +0300

    impl fetchFutureBook

---
+++ lib/features/home/data/data_source/home_remot_data_source.dart
@@ -0,0 +1,41 @@
+import 'package:bookly_app/core/utiles/api_service.dart';
+import 'package:bookly_app/features/home/data/models/BookModel.dart';
+
+import '../../domain/entites/book_entity.dart';
+
+abstract class HomeRemoteDataSource{
+  Future<List<BookEntity>> fetchFutureBook();
+  Future<List<BookEntity>> fetchNewFutureBook();
+
+
+
+}
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
+
+  @override
+  Future<List<BookEntity>> fetchFutureBook()async {
+   var  data = await apiService.get(endpoint: "volumes?q=computer science");
+    // TODO: implement fetchFutureBook
+    List<BookEntity> bokEntity = getbookslist(data);
+    return bokEntity;
+  }
+
+
+
+  @override
+  Future<List<BookEntity>> fetchNewFutureBook() {
+    // TODO: implement fetchNewFutureBook
+    throw UnimplementedError();
+  }
+  List<BookEntity> getbookslist(Map<String, dynamic> data) {
+    List<BookEntity> bokEntity=[];
+    for (var e in data['items']){
+      bokEntity.add(BookModel.fromJson(e));
+    }
+    return bokEntity;
+  }
+
+}
